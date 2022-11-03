5a57844ec7ebaf85058df2dd401a9c33bff0d006
Mouz24 <90099528+Mouz24@users.noreply.github.com>
2022-11-04 01:11:34 +0300

    Changed

--- Queries/FourthTask(2).sql
+++ Queries/FourthTask(2).sql
@@ -1,8 +1,7 @@
-USE Bank_Sphere
-SELECT DISTINCT Status,
-(
-    SELECT SUM(NumberOfCards)
-    FROM Client_Data b
-    where a.Status = b.Status
-) AmountOFCardsForEachStatus
-from Client_Data a
+SELECT Social_Status.Id AS SocialStatus, Social_Status.Current_Status,
+	   (SELECT COUNT(*)
+	    FROM Cards, Clients, Users 
+	    WHERE Cards.[User_Id] = Users.Id
+		  AND Users.Client_Id = Clients.Id
+		  AND Clients.Social_Status_Id = Social_Status.Id) AS AmountOfCards
+FROM Social_Status
