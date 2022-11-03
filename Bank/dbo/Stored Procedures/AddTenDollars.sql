c526cad49cbb15494553c2c81091f71ab43e1969
Mouz24 <90099528+Mouz24@users.noreply.github.com>
2022-11-04 01:14:40 +0300

    Changed

--- Bank/dbo/Stored Procedures/AddTenDollars.sql
+++ Bank/dbo/Stored Procedures/AddTenDollars.sql
@@ -1,22 +1,15 @@
-CREATE PROCEDURE ADD10DOLLARS @StatusID INT
+CREATE PROCEDURE AddTenDollars
+	   @Status VARCHAR(25)
 AS 
-BEGIN TRY
-IF (@StatusID > (SELECT COUNT(ClientID) FROM Client_Data) OR @StatusID = 0)
-BEGIN 
- RAISERROR ('There is no such status', 16, 1)
+BEGIN
+    IF (@Status != ALL(SELECT Current_Status FROM Social_Status))
+        RAISERROR ('There is no such status', 16, 1)
+
+    UPDATE Users
+    SET Balance = Balance + 10.00
+    WHERE Users.Id = ANY(SELECT Users.Id
+					     FROM Social_Status, Clients, Users
+					     WHERE Social_Status.Current_Status = @Status
+						    AND Users.Client_Id = Clients.Id
+						    AND Social_Status.id = Clients.Social_Status_Id)
 END
-UPDATE Client_Data
-SET AccountMoney = AccountMoney + 10.00
-WHERE Status = @StatusID
-END TRY
-BEGIN CATCH
- DECLARE @ErrorMsg nvarchar(240)
-DECLARE @ErrorSeverity INT
-DECLARE @ErrState INT
-
-SELECT @ErrorMsg = ERROR_MESSAGE(),
-       @ErrorSeverity = ERROR_SEVERITY(),
-	   @ErrState = ERROR_STATE();
-
-RAISERROR (@ErrorMsg, @ErrorSeverity, @ErrState)
-END CATCH
