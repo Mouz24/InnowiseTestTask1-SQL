262f83271b4134d4343eb2b7c7d8a52f9ea32421
Mouz24 <90099528+Mouz24@users.noreply.github.com>
2022-11-04 22:06:59 +0300

    Changed

--- Bank/dbo/Stored Procedures/AddTenDollarsExecution.sql
+++ Bank/dbo/Stored Procedures/AddTenDollarsExecution.sql
@@ -1,12 +1,11 @@
-USE [Bank_Sphere]
-GO
-
 SELECT Clients.FirstName, Clients.SecondName, Users.Balance, Bank.Bank_Name
 FROM Clients
 JOIN Users ON Users.Client_Id = Clients.Id
 
 JOIN (SELECT Id, Bank_Name 
 		  FROM Banks) AS Bank ON Bank.Id = Clients.Bank_Id
+
+GO
 
 DECLARE	@return_value int
 
