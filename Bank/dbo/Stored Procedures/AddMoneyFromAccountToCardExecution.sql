6ab2aedf16be46bfdaed69fb22a06946c4736de1
Mouz24 <90099528+Mouz24@users.noreply.github.com>
2022-11-04 22:08:21 +0300

    Changed

--- Bank/dbo/Stored Procedures/AddMoneyFromAccountToCardExecution.sql
+++ Bank/dbo/Stored Procedures/AddMoneyFromAccountToCardExecution.sql
@@ -1,11 +1,10 @@
-USE [Bank_Sphere]
-GO
-
 SELECT Clients.FirstName, Clients.SecondName, Cards.Number, Users.Balance, Cards.Balance AS CardBalance
 FROM Clients 
 JOIN Users ON Users.Client_Id = Clients.Id
 
 JOIN Cards ON Cards.[User_Id] = Users.Id
+
+GO
 
 DECLARE	@return_value int
 
