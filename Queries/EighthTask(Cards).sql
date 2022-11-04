bb3db0e2949a6478e9557680b913e759526d9bd6
Mouz24 <90099528+Mouz24@users.noreply.github.com>
2022-11-04 22:03:06 +0300

    Changed

--- Queries/EighthTask(Cards).sql
+++ Queries/EighthTask(Cards).sql
@@ -1,16 +1,46 @@
-CREATE TRIGGER Card_Balance_Change_Controller
-ON Cards 
-AFTER INSERT,UPDATE
-AS 
-IF UPDATE (MoneyOnCard)
-BEGIN 
-UPDATE Client_Data
-SET MoneyOnCards = (SELECT SUM(MoneyOnCard) FROM Cards WHERE ClientID = Client_FK) 
-IF EXISTS (SELECT * FROM Client_Data,Cards WHERE ClientID = Client_FK AND AccountMoney < MoneyOnCards)
- BEGIN 
-  ROLLBACK TRAN
-  RAISERROR ('There is no money available for such a transaction', 16, 1)
- END
+CREATE TRIGGER CardBalanceUpdateControl
+ON Cards
+INSTEAD OF UPDATE
+AS BEGIN
+    DECLARE @UserBalance DECIMAL(18,2), 
+		    @CardBalanceBeforeUpdate DECIMAL(18,2), 
+		    @CardBalanceAfterUpdate DECIMAL(18,2),
+		    @CurrentTotalCardsBalance DECIMAL(18,2), 
+		    @ResultTotalCardsBalance DECIMAL(18,2)
+
+    SELECT @UserBalance = (SELECT Balance
+						   FROM Users
+						   WHERE Users.Id = (SELECT [User_Id] 
+										     FROM inserted))
+
+	SELECT @CurrentTotalCardsBalance = (SELECT SUM(Cards.Balance)
+										FROM Cards
+										WHERE Cards.[User_Id] =	(SELECT [User_Id] 
+																 FROM inserted)
+										GROUP BY Cards.[User_Id])
+
+	SELECT @CardBalanceAfterUpdate = (SELECT Balance
+									  FROM inserted)
+
+	SELECT @CardBalanceBeforeUpdate = (SELECT Balance
+									   FROM Cards
+									   WHERE Cards.Id = (SELECT Id 
+														 FROM inserted))
+
+	SELECT @ResultTotalCardsBalance = @CurrentTotalCardsBalance - @CardBalanceBeforeUpdate + @CardBalanceAfterUpdate
+
+	UPDATE Cards
+	SET Balance = @CardBalanceAfterUpdate
+	WHERE Cards.Id = (SELECT Id 
+					  FROM inserted)
+		AND @UserBalance >= @ResultTotalCardsBalance
+
+	IF (@@ROWCOUNT = 0)
+		RAISERROR('There is no money available for transaction', 16, 1)
+	ELSE
+		PRINT 'Card balance was updated successfully'
 END
+
 GO
-ENABLE TRIGGER Card_Balance_Change_Controller ON Cards
+
+ENABLE TRIGGER CardBalanceUpdateControl ON Cards
