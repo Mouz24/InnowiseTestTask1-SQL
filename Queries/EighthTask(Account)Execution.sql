d350ddc580ae1679098398ce15ac15cefea23945
Mouz24 <90099528+Mouz24@users.noreply.github.com>
2022-11-04 22:05:16 +0300

    Changed

--- Queries/EighthTask(Account)Execution.sql
+++ Queries/EighthTask(Account)Execution.sql
@@ -4,8 +4,12 @@ JOIN Users ON Users.Client_Id = Clients.Id
 
 JOIN Cards ON Cards.[User_Id] = Users.Id
 
+GO
+
 UPDATE Users
 SET Balance = 300.00 WHERE Client_Id = 4
+
+GO
 
 SELECT Clients.FirstName, Clients.SecondName, Cards.Number, Users.Balance, Cards.Balance AS CardBalance
 FROM Clients 
