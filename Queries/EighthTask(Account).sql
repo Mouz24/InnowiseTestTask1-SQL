8acecbbbb2bbd31b9f3b1ef5aa1ef2386e4e54ce
Mouz24 <90099528+Mouz24@users.noreply.github.com>
2022-11-04 22:02:15 +0300

    Changed

--- Queries/EighthTask(Account).sql
+++ Queries/EighthTask(Account).sql
@@ -1,17 +1,30 @@
-CREATE TRIGGER Control_Balance_On_Account
-ON Client_Data
-AFTER INSERT,UPDATE
-AS 
-IF UPDATE (AccountMoney)
-BEGIN 
- IF EXISTS (
- SELECT * FROM inserted I, Client_Data P 
- WHERE (I.AccountMoney < I.MoneyOnCards) OR (I.AccountMoney < P.MoneyOnCards AND I.ClientID = P.ClientID)
- )
-  BEGIN 
-   ROLLBACK TRAN
-   RAISERROR ('Account balance is less than money on cards', 16, 1)
-  END
+CREATE TRIGGER UserBalanceChangeControl
+ON Users
+INSTEAD OF UPDATE
+AS BEGIN
+	DECLARE @ChangedBalance DECIMAL(18, 2), 
+	        @TotalCardsBalance DECIMAL(18, 2)
+
+	SELECT @ChangedBalance = (SELECT Balance 
+							  FROM inserted)
+
+	SELECT @TotalCardsBalance = (SELECT SUM(Cards.Balance) AS TotalCardsBalance
+								 FROM Cards
+								 WHERE Cards.[User_Id] = (SELECT Id 
+														  FROM inserted)
+								 GROUP BY Cards.[User_Id])
+	UPDATE Users
+	SET Balance = @ChangedBalance
+	WHERE Users.Id = ALL(SELECT Id 
+						FROM inserted)
+		AND @ChangedBalance >= @TotalCardsBalance
+
+	IF @@ROWCOUNT = 0
+		RAISERROR ('Account balance is less than total balance on cards', 16, 1)
+	ELSE
+		PRINT 'Balance was updated successfully'
 END
+
 GO
-ENABLE TRIGGER Control_Balance_On_Account ON Client_Data
+
+ENABLE TRIGGER UserBalanceChangeControl ON Users
